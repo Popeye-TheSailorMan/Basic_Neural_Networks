bd4ab95755fbe3105ebfedfab3cee5f762e172e6
Abhisheksaxena <44522820+Abhisheksax@users.noreply.github.com>
2022-06-01 01:08:59 +0530

    Completed vgg16

--- WritingNNDaily.py
+++ WritingNNDaily.py
@@ -6,7 +6,6 @@ import numpy as np
 # ====== DAY - 1 ======
 # 1. VGG16
 vgg16 = Sequential()
-
 vgg16.add(Conv2D(input_shape=(224,224),filters = 64, kernel_size = (3,3)
           ,padding = "same", activation = "relu"))
 vgg16.add(Conv2D(filters = 64, kernel_size=(3,3),padding ="same",
@@ -16,5 +15,32 @@ vgg16.add(Conv2D(filters = 128, kernel_size = (3,3),padding = "same"
                  , activation = "relu"))
 vgg16.add(Conv2D(filters = 128, kernel_size= (3,3), padding = "same",
                  activation = "relu"))
+vgg16.add(MaxPool2D(pool_size=(2,2),strides=(2,2)))
+vgg16.add(Conv2D(filters = 256, kernel_size = (3,3), padding = "same",
+                 activation = "relu"))
+vgg16.add(Conv2D(filters = 256, kernel_size=(3,3), padding = "same",
+                 activation= "relu"))
+vgg16.add(Conv2D(filters = 256, kernel_size= (3,3), padding= "same",
+                 activation= "relu"))
+vgg16.add(MaxPool2D(pool_size=(2,2),strides=(2,2)))
+vgg16.add(Conv2D(filters=512, kernel_size = (3,3), padding = "same",
+                 activation ="relu"))
+vgg16.add(Conv2D(filters=512, kernel_size = (3,3), padding = "same",
+                 activation ="relu"))
+vgg16.add(Conv2D(filters=512, kernel_size = (3,3), padding = "same",
+                 activation ="relu"))
+vgg16.add(MaxPool2D(pool_size=(2,2), strides =(2,2)))
+vgg16.add(Conv2D(filters=512, kernel_size = (3,3), padding = "same",
+                 activation ="relu"))
+vgg16.add(Conv2D(filters=512, kernel_size = (3,3), padding = "same",
+                 activation ="relu"))
+vgg16.add(Conv2D(filters=512, kernel_size = (3,3), padding = "same",
+                 activation ="relu"))
+vgg16.add(MaxPool2D(pool_size=(2,2), strides = (2,2)))
+vgg16.add(Flatten())
+vgg16.add(Dense(units = 4096,activation="relu"))
+vgg16.add(Dense(units = 4096, activation = "relu"))
+vgg16.add(Dense(units = 2, activation="softmax"))
+
 
 
