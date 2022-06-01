a06e91aa961e4eeb09ea0be657df19fec8e8fd02
Abhisheksaxena <44522820+Abhisheksax@users.noreply.github.com>
2022-06-01 23:23:39 +0530

    Day2 vgg19 added

--- WritingNNDaily.py
+++ WritingNNDaily.py
@@ -42,5 +42,48 @@ vgg16.add(Dense(units = 4096,activation="relu"))
 vgg16.add(Dense(units = 4096, activation = "relu"))
 vgg16.add(Dense(units = 2, activation="softmax"))
 
+# DAY - 2
+# ===== VGG19 =======
 
+vgg19 = Sequential()
+vgg19.add(Conv2D(input_shape=(224,224), filters= 64, kernel_size=(3,3),
+                 padding="same",activation="relu"))
+vgg19.add(Conv2D(filters=64, kernel_size=(3,3), padding="same",
+                 activation="relu"))
+vgg19.add(MaxPool2D(pool_size = (2,2), strides = (2,2)))
+vgg19.add(Conv2D(filters = 128, kernel_size=(3,3), padding = "same",
+                 activation="relu"))
+vgg19.add(Conv2D(filters = 128, kernel_size=(3,3),padding = "same",
+                 activation = "relu"))
+vgg19.add(MaxPool2D(pool_size = (2,2), strides = (2,2)))
+vgg19.add(Conv2D(filters = 256, kernel_size = (3,3), padding = "same",
+                 activation="relu"))
+vgg19.add(Conv2D(filters = 256, kernel_size = (3,3), padding = "same",
+                 activation="relu"))
+vgg19.add(Conv2D(filters = 256, kernel_size = (3,3), padding = "same",
+                 activation="relu"))
+vgg19.add(Conv2D(filters = 256, kernel_size = (3,3), padding = "same",
+                 activation="relu"))
+vgg19.add(MaxPool2D(pool_size = (2,2), strides = (2,2)))
+vgg19.add(Conv2D(filters = 512, kernel_size = (3,3), padding = "same",
+                 activation="relu"))
+vgg19.add(Conv2D(filters = 512, kernel_size = (3,3), padding = "same",
+                 activation="relu"))
+vgg19.add(Conv2D(filters = 512, kernel_size = (3,3), padding = "same",
+                 activation="relu"))
+vgg19.add(Conv2D(filters = 512, kernel_size = (3,3), padding = "same",
+                 activation="relu"))
+vgg19.add(MaxPool2D(pool_size=(2,2), strides=(2,2)))
+vgg19.add(Conv2D(filters = 512, kernel_size = (3,3), padding = "same",
+                 activation="relu"))
+vgg19.add(Conv2D(filters = 512, kernel_size = (3,3), padding = "same",
+                 activation="relu"))
+vgg19.add(Conv2D(filters = 512, kernel_size = (3,3), padding = "same",
+                 activation="relu"))
+vgg19.add(Conv2D(filters = 512, kernel_size = (3,3), padding = "same",
+                 activation="relu"))
+vgg19.add(Flatten())
+vgg19.add(Dense(4096,activation = "relu"))
+vgg19.add(Dense(4096,activation = "relu"))
+vgg19.add(Dense(2, activation="softmax"))
 
